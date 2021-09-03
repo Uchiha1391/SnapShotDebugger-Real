4d304ef21766f47d9ecbbe84042e4b5126e2318c
Karan <jkaran.sharma101@gmail.com>
2021-09-03 17:42:30 +0530

    Update SnapshotInjector.cs
    Now snapshot injector can detect automatically compilation and inject code which was not working before

--- SnapShotDebuggerScripts/SnapshotInjector.cs
+++ SnapShotDebuggerScripts/SnapshotInjector.cs
@@ -16,27 +16,36 @@ namespace Assets.Editor
 {
     static class SnapshotInjector
     {
-        public static string AssemblyLocation = "";
-        public static Type TypeToGenerate=null;
+        private static string AssemblyLocationToAllow;
+        
+        public static Type TypeToGenerate = null;
 
+
+
+
+        private static void FillAssemblyName()
+        {
+            AssemblyLocationToAllow = typeof(testScript).Assembly.Location;
+
+        }
         [InitializeOnLoadMethod]
         private static void OnInitialized()
         {
-            //AssemblyReloadEvents.afterAssemblyReload += InjectCode;
-            //CompilationPipeline.compilationFinished += InjectCode;
-            CompilationPipeline.assemblyCompilationFinished +=
-                OnCompilationFinished;
+            Debug.Log("Snapshot Injection Callback Initialized");
+
+            if (!EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                CompilationPipeline.assemblyCompilationFinished += OnCompilationFinished;
+            }
         }
 
-        private static bool CompilerMessagesContainError(
-            CompilerMessage[] messages)
+        private static bool CompilerMessagesContainError(CompilerMessage[] messages)
         {
             return messages.Any(msg => msg.type == CompilerMessageType.Error);
         }
-        //
+        
 
-        private static void OnCompilationFinished(string assemblyPath,
-            CompilerMessage[] messages)
+        private static void OnCompilationFinished(string assemblyPath, CompilerMessage[] messages)
         {
             // Do nothing if there were compile errors on the target
             if (CompilerMessagesContainError(messages))
@@ -45,67 +54,37 @@ namespace Assets.Editor
                 return;
             }
 
+            FillAssemblyName();
             // its kinda useless code for now its just for testing string comparison
-            if (assemblyPath.Contains("-Editor") ||
-                assemblyPath.Contains(".Editor") ||
+            if (assemblyPath.Contains("-Editor") || assemblyPath.Contains(".Editor") ||
                 assemblyPath.Contains("Editor.dll"))
                 return;
 
-            if (assemblyPath.Contains("NewgameAssembly.dll"))
+            var normalizeScriptPath = AssemblyLocationToAllow.Replace('\\', '/');
+            var RelativePath = FileUtil.GetProjectRelativePath(normalizeScriptPath);
+
+
+            if (assemblyPath.Contains(RelativePath))
+            {
                 InjectCode();
+            }
+           
         }
-        //
-        //
-        //[InitializeOnLoad]
-        //public class Startup
-        //{
-        //    static Startup()
-        //    {
-        //        CompilationPipeline.assemblyCompilationFinished += CallInjector;
-        //    }
 
-        //    private static void CallInjector(string assemblyPath, CompilerMessage[] arg2)
-        //    {
-        //        var res = arg2.Any(msg => msg.type == CompilerMessageType.Error);
-
-        //        if (res)
-        //        {
-        //            Debug.Log("snapshot injector: stop because compile errors on target");
-        //            return;
-        //        }
-
-        //        // Should not run on the editor only assemblies
-        //        if (assemblyPath.Contains("-Editor") || assemblyPath.Contains(".Editor"))
-        //        {
-        //            return;
-        //        }
-
-        //        InjectCode();
-        //    }////
-        //}
-        /// <summary>
-        ///     /
-        /// </summary>
-        /// <param name="o"></param>
-        //
         [MenuItem("My Ui Commands/injectMethod #&q")]
         public static void InjectCode()
         {
-                var AssemblyFilePath = AssemblyLocation;
-            using (var AssemblyDefinitionInstance =
-                AssemblyDefinition.ReadAssembly(AssemblyFilePath,
-                    new ReaderParameters
-                        {ReadWrite = true, ReadSymbols = true}))
+            var AssemblyFilePath = AssemblyLocationToAllow;
+            using (var AssemblyDefinitionInstance = AssemblyDefinition.ReadAssembly(
+                AssemblyFilePath, new ReaderParameters {ReadWrite = true, ReadSymbols = true}))
             {
-                var TypeDefinitions = AssemblyDefinitionInstance.MainModule
-                    .GetTypes()
-                    .Where(definition =>
+                var TypeDefinitions = AssemblyDefinitionInstance.MainModule.GetTypes().Where(
+                    definition =>
                     {
                         var att = definition.CustomAttributes;
                         foreach (var CustomAttribute in att)
                             if (CustomAttribute.AttributeType.Name ==
-                                nameof(SnapShotAttributes
-                                    .SnapShotInjectionAttribute))
+                                nameof(SnapShotAttributes.SnapShotInjectionAttribute))
                                 return true;
 
                         return false;
@@ -120,8 +99,7 @@ namespace Assets.Editor
                         var at = definition.CustomAttributes;
                         foreach (var CustomAttribute in at)
                             if (CustomAttribute.AttributeType.Name ==
-                                nameof(SnapShotAttributes
-                                    .IgnoreSnapShotInjectionAttribute))
+                                nameof(SnapShotAttributes.IgnoreSnapShotInjectionAttribute))
                                 return false;
 
                         return true;
@@ -132,8 +110,7 @@ namespace Assets.Editor
                     var dd = TypeDefinition.Methods.ToList();
                     foreach (var MethodDefinition in dd)
                     {
-                        var IsMethodEligible =
-                            CheckforAttributes(MethodDefinition);
+                        var IsMethodEligible = CheckforAttributes(MethodDefinition);
                         if (!IsMethodEligible) continue;
                         MethodsTOinject.Add(MethodDefinition);
                     }
@@ -143,85 +120,64 @@ namespace Assets.Editor
                     AssemblyDefinitionInstance.MainModule.GetType(
                         typeof(SnapshotDebubber).ToString());
                 var InstructionsOfMethodToinjectEnurmator =
-                    InjectMethodType.Methods.Single(definition =>
-                        definition.Name == "OnEntry");
-                var TakeSnapshotForParametersMethod =
-                    InjectMethodType.Methods.Single(definition =>
-                        definition.Name == "TakeSnapshotForParameters");
+                    InjectMethodType.Methods.Single(definition => definition.Name == "OnEntry");
+                var TakeSnapshotForParametersMethod = InjectMethodType.Methods.Single(definition =>
+                    definition.Name == "TakeSnapshotForParameters");
 
                 var InstructionsOfMethodToinjectList =
-                    InstructionsOfMethodToinjectEnurmator.Body.Instructions
-                        .ToList();
+                    InstructionsOfMethodToinjectEnurmator.Body.Instructions.ToList();
                 InstructionsOfMethodToinjectList.Reverse();
                 InstructionsOfMethodToinjectList.RemoveAt(0);
 
-                if (MethodsTOinject != null)
+                if (MethodsTOinject.Count != 0)
                 {
-                    var FilterMethodList =
-                        FilterMethodDefinitions(MethodsTOinject.ToList());
+                    var FilterMethodList = FilterMethodDefinitions(MethodsTOinject.ToList());
 
                     foreach (var MethodDefinition in FilterMethodList)
 
                     {
                         #region parameter il code
 
-                        var arrayDef =
-                            new VariableDefinition(
-                                new ArrayType(AssemblyDefinitionInstance
-                                    .MainModule.TypeSystem
-                                    .Object)); // create variable to hold the array to be passed to the LogEntry() method    
+                        var arrayDef = new VariableDefinition(
+                            new ArrayType(AssemblyDefinitionInstance.MainModule.TypeSystem
+                                .Object)); // create variable to hold the array to be passed to the LogEntry() method    
 
                         MethodDefinition.Body.Variables
-                            .Add(
-                                arrayDef); // add variable to the method          
+                            .Add(arrayDef); // add variable to the method          
 
-                        var IlProcessor =
-                            MethodDefinition.Body.GetILProcessor();
+                        var IlProcessor = MethodDefinition.Body.GetILProcessor();
                         var ParametersInstructions = new List<Instruction>();
 
-                        ParametersInstructions.Add(
-                            IlProcessor.Create(OpCodes.Ldc_I4,
-                                MethodDefinition.Parameters
-                                    .Count)); // load to the stack the number of parameters                      
-                        ParametersInstructions.Add(IlProcessor.Create(
-                            OpCodes.Newarr,
+                        ParametersInstructions.Add(IlProcessor.Create(OpCodes.Ldc_I4,
+                            MethodDefinition.Parameters
+                                .Count)); // load to the stack the number of parameters                      
+                        ParametersInstructions.Add(IlProcessor.Create(OpCodes.Newarr,
                             AssemblyDefinitionInstance.MainModule.TypeSystem
                                 .Object)); // create a new object[] with the number loaded to the stack           
-                        ParametersInstructions.Add(IlProcessor.Create(
-                            OpCodes.Stloc,
+                        ParametersInstructions.Add(IlProcessor.Create(OpCodes.Stloc,
                             arrayDef)); // store the array in the local variable
 
                         // loop through the parameters of the method to run
-                        for (var i = 0;
-                            i < MethodDefinition.Parameters.Count;
-                            i++)
+                        for (var i = 0; i < MethodDefinition.Parameters.Count; i++)
                         {
-                            ParametersInstructions.Add(IlProcessor.Create(
-                                OpCodes.Ldloc,
+                            ParametersInstructions.Add(IlProcessor.Create(OpCodes.Ldloc,
                                 arrayDef)); // load the array from the local variable
-                            ParametersInstructions.Add(
-                                IlProcessor.Create(OpCodes.Ldc_I4,
-                                    i)); // load the index
-                            ParametersInstructions.Add(IlProcessor.Create(
-                                OpCodes.Ldarg,
+                            ParametersInstructions.Add(IlProcessor.Create(OpCodes.Ldc_I4,
+                                i)); // load the index
+                            ParametersInstructions.Add(IlProcessor.Create(OpCodes.Ldarg,
                                 i + 1)); // load the argument of the original method (note that parameter 0 is 'this', that's omitted)
 
-                            if (MethodDefinition.Parameters[i].ParameterType
-                                .IsValueType)
-                                ParametersInstructions.Add(IlProcessor.Create(
-                                    OpCodes.Box,
+                            if (MethodDefinition.Parameters[i].ParameterType.IsValueType)
+                                ParametersInstructions.Add(IlProcessor.Create(OpCodes.Box,
                                     MethodDefinition.Parameters[i]
                                         .ParameterType)); // boxing is needed for value types
                             else
-                                ParametersInstructions.Add(IlProcessor.Create(
-                                    OpCodes.Castclass,
-                                    AssemblyDefinitionInstance.MainModule
-                                        .TypeSystem
+                                ParametersInstructions.Add(IlProcessor.Create(OpCodes.Castclass,
+                                    AssemblyDefinitionInstance.MainModule.TypeSystem
                                         .Object)); // casting for reference types
 
                             ParametersInstructions.Add(
-                                IlProcessor.Create(OpCodes
-                                    .Stelem_Ref)); // store in the array
+                                IlProcessor.Create(OpCodes.Stelem_Ref)); // store in the array
                         }
 
                         #endregion
@@ -233,47 +189,45 @@ namespace Assets.Editor
                         #region for calling parameter method
 
                         // beware of sequence of instruction  right now its correct
-                        FinalInstructionsToinject.Add(IlProcessor.Create(
-                            OpCodes.Call,
+                        FinalInstructionsToinject.Add(IlProcessor.Create(OpCodes.Call,
                             TakeSnapshotForParametersMethod)); // call the LogEntry() method
-                        FinalInstructionsToinject.Add(IlProcessor.Create(
-                            OpCodes.Ldloc,
+                        FinalInstructionsToinject.Add(IlProcessor.Create(OpCodes.Ldloc,
                             arrayDef)); // load the array to the stack
 
                         #endregion
 
-                        FinalInstructionsToinject.AddRange(
-                            ParametersInstructions);
+                        FinalInstructionsToinject.AddRange(ParametersInstructions);
 
                         //
-                        FinalInstructionsToinject.AddRange(
-                            InstructionsOfMethodToinjectList);
+                        FinalInstructionsToinject.AddRange(InstructionsOfMethodToinjectList);
 
 
-                        foreach (var newInstruction in FinalInstructionsToinject
-                        ) // add the new instructions in referse order
+                        foreach (var newInstruction in
+                            FinalInstructionsToinject) // add the new instructions in referse order
                         {
-                            var firstInstruction =
-                                MethodDefinition.Body.Instructions[0];
-                            var processor =
-                                MethodDefinition.Body.GetILProcessor();
-                            processor.InsertBefore(firstInstruction,
-                                newInstruction);
+                            var firstInstruction = MethodDefinition.Body.Instructions[0];
+                            var processor = MethodDefinition.Body.GetILProcessor();
+                            processor.InsertBefore(firstInstruction, newInstruction);
                         }
 
                         MethodDefinition.Body.Optimize();
                     }
+
+                    var writeParams = new WriterParameters {WriteSymbols = true};
+
+                    AssemblyDefinitionInstance
+                        .Write(writeParams); // Write to the same file that was used to open the file
+
+
+                    Debug.Log(" finished injecting methods count==" + FilterMethodList.Count);
                 }
-
-                var writeParams = new WriterParameters {WriteSymbols = true};
-
-                AssemblyDefinitionInstance
-                    .Write(
-                        writeParams); // Write to the same file that was used to open the file
+                else
+                {
+                    Debug.Log("No methods to inject ");
+                }
             }
 
             //
-            Debug.Log(" finished injection");
         }
 
         public static List<MethodDefinition> FilterMethodDefinitions(
@@ -284,8 +238,8 @@ namespace Assets.Editor
             foreach (var MethodDefinition in rawMethodDefinitionsList)
             {
                 if (MethodDefinition.IsSetter || MethodDefinition.IsGetter ||
-                    MethodDefinition.IsConstructor ||
-                    MethodDefinition.IsSpecialName || MethodDefinition.IsStatic)
+                    MethodDefinition.IsConstructor || MethodDefinition.IsSpecialName ||
+                    MethodDefinition.IsStatic)
                     continue;
 
                 filteredList.Add(MethodDefinition);
@@ -300,8 +254,7 @@ namespace Assets.Editor
             var TypeToCreateScriptFor = TypeToGenerate;
 
             CustomEs3TypeGeneratorData.Instance.fields =
-                ES3Reflection.GetSerializableMembers(TypeToCreateScriptFor,
-                    false);
+                ES3Reflection.GetSerializableMembers(TypeToCreateScriptFor, false);
 
             CustomEs3TypeGeneratorData.Instance.fieldSelected =
                 new bool[CustomEs3TypeGeneratorData.Instance.fields.Length];
@@ -315,25 +268,12 @@ namespace Assets.Editor
             //    customEs3TypeGeneratorData.Instance.fieldSelected[Index] = true;
             //}
 
-            CustomEs3TypeGeneratorData.Instance.GenerateCustom(
-                TypeToCreateScriptFor);
+            CustomEs3TypeGeneratorData.Instance.GenerateCustom(TypeToCreateScriptFor);
 
             //customEs3TypeGeneratorData.Instance.SelectType(TypeToCreateScriptFor); // doesnt need this it servers no purpose
             Debug.Log("finished es3 type");
         }
 
 
-        [MenuItem("My Ui Commands/pdbgenretate #&q")]
-        public static void TestPDBTOMDB()
-        {
-            var type = Type.GetType("Mono.Runtime");
-            if (type != null)
-            {
-                var displayName = type.GetMethod("GetDisplayName",
-                    BindingFlags.NonPublic | BindingFlags.Static);
-                if (displayName != null)
-                    Debug.Log(displayName.Invoke(null, null));
-            }
-        }
     }
 }

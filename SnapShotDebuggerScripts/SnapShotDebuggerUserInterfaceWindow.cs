4972beffbcb4cc9e4bf9912ad8006621e31287b4
Karan <jkaran.sharma101@gmail.com>
2022-05-06 15:13:41 +0530

    big changes, new code and its working.... I wont write all the changes but its working nicely

---
+++ SnapShotDebuggerScripts/SnapShotDebuggerUserInterfaceWindow.cs
@@ -0,0 +1,58 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using NewGame;
+using RoslynCSharp.HotReloading;
+using Sirenix.OdinInspector;
+using UnityEditor;
+
+public class SnapShotDebuggerUserInterface : OdinPracticeEditorWindow
+{
+
+
+    [MenuItem("My Ui Commands/SnapShotDebuggerUserInterface ")]
+    public static void OpenWindow()
+    {
+        
+        GetWindow<SnapShotDebuggerUserInterface>().Show();
+    }
+
+    [ShowInInspector]
+    public bool SnapShotStart
+    {
+        get => SnapshotDebubber.ShouldTakeSnapShot;
+        set => SnapshotDebubber.ShouldTakeSnapShot = value;
+    }
+
+    [ShowInInspector]
+    public bool RestartHotReloading
+    {
+        get => RealtimeScriptWatcher.RestartHotReloading;
+        set => RealtimeScriptWatcher.RestartHotReloading = value;
+    }
+
+
+    [Button]
+    public void GetSizeAllocatedByDebuggeer()
+    {
+        SnapshotDebubber.SizeofDebuggingData();
+    }
+
+    [Button]
+    public void UndoMethodlast()
+    {
+        SnapshotDebubber.UndoLastMethod();
+        Debug.LogError("not implemnted right now");
+    }
+
+    [Button]
+    public void OverwriteOrDeleteTempFile()
+    {
+        RealtimeScriptingService.watcher.OverwriteOrDeleteTempFile();
+    }
+
+}
+#endif
